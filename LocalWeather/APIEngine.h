fce1063ce46a8b8f6e2d196204811ab1946e70aa
LAP11961 <maihuuchung@gmail.com>
2018-07-07 22:27:49 +0700

    v4.23

--- LocalWeather/APIEngine.h
+++ LocalWeather/APIEngine.h
@@ -22,7 +22,7 @@
 // Feature 1 New Update 1.2
 // Feature 1 New Update 2.2
 // Feature 1 New Update 4.21
-// Feature 1 New Update 4.13
+// Feature 1 New Update 4.23
 // Feature 1 New Update 4.14
 // Feature 1 New Update 4.15
 @end
